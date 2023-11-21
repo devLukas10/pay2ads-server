9060579c3b38c42d1a7015c8fec09371587a6135
Fernando Simão Lucas <102396021+lukas102000@users.noreply.github.com>
2023-11-21 07:21:22 +0100

    change-now

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
-from db.conexion import DBModuls, db, db_conexions
-import uvicorn, time
-
-
+import uvicorn, time, sqlite3
 app = FastAPI()
-db_moduls = DBModuls()
+db_conexions = sqlite3.connect('./pay2ads.db', 1000)
+db = db_conexions.cursor()
 
 #   add midwares
 app.add_middleware(
@@ -15,7 +13,59 @@ app.add_middleware(
     allow_credentials=True,
     allow_origins=['*']
 )
+
+# create users table
+db.execute("""
+    CREATE TABLE IF NOT EXISTS users 
+    (
+        id INTEGER PRIMARY_KEY AUTO_INCREMENT, 
+        username TEXT, email TEXT, reference_link TEXT, my_reference_link TEXT,
+        balance TEXT, reference_bonus TEXT, clicks TEXT,
+        reference_count TEXT, password TEXT, created_at TEXT
+    )
+""")
+db.execute("""
+    CREATE TABLE IF NOT EXISTS users_marketing 
+    (
+        id TEXT, username TEXT, link TEXT, posted_at TEXT
+    )
+""")
+db.execute("""
+    CREATE TABLE IF NOT EXISTS users_payment_forms 
+    (
+        id TEXT, username TEXT, back_name TEXT, iban TEXT, number TEXT, valor TEXT, created_at TEXT
+    )
+""")
+
+class DBModuls:
+    # create find All methodes
+    def findAll (self, query):
+        db.execute(f"SELECT * FROM {query}")
+        result = db.fetchall()
+        column = [desc[0] for desc in db.description]
+        converts_to_json = [dict(zip(column, row)) for row in result]
+        return converts_to_json
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # ============================ post methods =============================================
+db_moduls = DBModuls()
+
+
 @app.get('/api')
 async def api(req: Request):
     return {"sms": "api is building..."}
