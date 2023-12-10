033edac2a636cf773e0c7e7e668e8948b9667d69
Fernando Simão Lucas <102396021+lukas102000@users.noreply.github.com>
2023-12-10 19:56:53 +0100

    getting-user-data

--- main.py
+++ main.py
@@ -318,8 +318,26 @@ try:
         return {"data": data}
 
 
-
-
+    # ============================ post methods =============================================
+    # router to create new user accounts
+    @app.post('/app_pay2ads_get_user_inbox_count_data')
+    async def app_pay2ads_get_user_inbox_count_data(req: Request):
+        res = await req.json()
+        ids = res['id']
+        data = db_moduls.findAll(f"refers_list WHERE id='{ids}' ")
+        return {"data": data}
+    @app.post('/app_pay2ads_get_user_inbox_data')
+    async def app_pay2ads_get_user_inbox_data(req: Request):
+        res = await req.json()
+        ids = res['id']
+        data = db_moduls.findAll(f"ibonx WHERE id='{ids}' ")
+        return {"data": data}
+    @app.post('/app_pay2ads_get_user_refers_data')
+    async def app_pay2ads_get_user_refers_data(req: Request):
+        res = await req.json()
+        ids = res['id']
+        data = db_moduls.findAll(f"refers_list id my_reference_link='{ids}' ")
+        return {"data": data}
 
 
 
